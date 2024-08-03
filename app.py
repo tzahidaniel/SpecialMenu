5a2471edef7c9de8fc42bbd8de0e798a87d60939
tzahi <tzahi.daniel@gmail.com>
2024-08-03 22:18:55 +0300

    adding main app

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+starters = ["Soup", "Salad", "Bruschetta", "Wings", "Cheese Balls", "Carpaccio", "Garlic Bread", "Smash Potatos", "Egg Rolls"]
+main_dishes = ["Steak", "Pasta", "Pizza", "Burger", "Shawarma", "Lazanya", "Wrappers", "Rice And Meat", "Falafel"]
+desserts = ["Ice Cream", "Cake", "Fruit Salad", "Souffle", "Donut", "Jello", "Macaroons"]
+
+@app.route('/')
+def home():
+    starter = random.choice(starters)
+    main_dish = random.choice(main_dishes)
+    dessert = random.choice(desserts)
+    return render_template('index.html', starter=starter, main_dish=main_dish, dessert=dessert)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
